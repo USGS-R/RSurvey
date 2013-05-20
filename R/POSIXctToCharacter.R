581c2cd53f257e8a15d6221cf6c8391f593891b5
Jason C Fisher <jfisher@usgs.gov>
2013-05-20 10:31:26 -0600

    Minor change

--- R/POSIXctToCharacter.R
+++ R/POSIXctToCharacter.R
@@ -4,11 +4,11 @@ POSIXctToCharacter <- function(obj, fmt="%Y-%m-%d %H:%M:%OS3") {
   match.location <- gregexpr("%OS[[:digit:]]+", fmt)[[1]]
   if (match.location > 0) {
     dec.digits <- as.integer(substr(fmt,
-                                    match.location + 3,
+                                    match.location + 3L,
                                     match.location + attr(match.location,
                                                           "match.length")))
     obj <- as.POSIXlt(obj)
-    obj$sec <- round(obj$sec, dec.digits) + 10^(-dec.digits - 1)
+    obj$sec <- round(obj$sec, dec.digits) + 10^(-dec.digits - 1L)
   }
   return(format(obj, format=fmt))
 }
