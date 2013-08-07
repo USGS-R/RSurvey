f6469c1d485b6bd18636224b79c06fcca5825555
Jason C Fisher <jfisher@usgs.gov>
2013-08-07 15:43:59 -0600

    Entry type changed to object class

--- R/CheckEntry.R
+++ R/CheckEntry.R
@@ -1,51 +1,25 @@
 # Controls the character string content within a Tk entry widget.
 
-CheckEntry <- function (ent.typ, ent.str="") {
+CheckEntry <- function(obj.class, ent.str="") {
 
   if (ent.str == "")
-    return("")
+    return(ent.str)
 
-  chr <- unlist(strsplit(ent.str, split=""))
-
-  if (ent.typ == "numeric") {
-    accept.vals <- c(as.character(0:9), "-", "e", "E", ".")
-  } else if (ent.typ %in% "integer") {
-    accept.vals <- c(as.character(0:9), "-", "e", "E")
-  } else if (ent.typ == "logical") {
-    accept.vals <- c("T", "R", "U", "E", "F", "A", "L", "S")
-  } else if (ent.typ == "second") {
-    accept.vals <- c(as.character(0:9), ".")
-  } else if (ent.typ %in% c("hour", "minute")) {
-    accept.vals <- c(as.character(0:9))
-  } else if (ent.typ == "date") {
-    accept.vals <- c("a", "A", "b", "c", "C", "d", "D", "e", "E", "F", "g",
-                     "G", "h", "H", "I", "j", "k", "l", "m", "M", "n", "O",
-                     "p", "r", "R", "S", "t", "T", "u", "U", "V", "w", "W",
-                     "x", "X", "y", "Y", "z", "Z", "/", "-", ":", "%", "#",
-                     " ", as.character(0:9))
+  if ("numeric" %in% obj.class) {
+    accept.vals <- c(as.character(0:9), "-", "e", "E", ".", "N", "A")
+  } else if ("integer" %in% obj.class) {
+    accept.vals <- c(as.character(0:9), "-", "e", "E", "N", "A")
+  } else if ("logical" %in% obj.class) {
+    accept.vals <- c("T", "R", "U", "E", "F", "A", "L", "S", "N")
+  } else {
+    return(ent.str)
   }
 
+  chr <- unlist(strsplit(ent.str, split=""))
   if (all(chr %in% accept.vals))
     rtn <- ent.str
   else
     rtn <- paste(chr[chr %in% accept.vals], collapse="")
-
-  if (rtn == "")
-   return(rtn)
-
-  if (ent.typ == "hour") {
-    if (as.integer(rtn) > 23)
-      rtn <- "23"
-  } else if (ent.typ == "minute") {
-    if (as.integer(rtn) > 59)
-      rtn <- "59"
-  } else if (ent.typ == "second") {
-    if (as.numeric(rtn) < 0) {
-      rtn <- "0"
-    } else if (as.numeric(rtn) > 59.999) {
-      rtn <- "59.999"
-    }
-  }
 
   return(rtn)
 }
