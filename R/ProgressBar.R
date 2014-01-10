aa53694b34c1d492db6151300c01be1e09ac35a1
Jason C Fisher <jfisher@usgs.gov>
2014-01-10 13:20:02 -0700

    More updates to ProgressBar

--- R/ProgressBar.R
+++ R/ProgressBar.R
@@ -1,5 +1,5 @@
 # A progress bar for providing feedback; derived from
-# tcltk::tkProgressBar (v3.0.2; Tcl/Tk v8.5.8)
+# tcltk::tkProgressBar (v3.0.2)
 
 ProgressBar <- function (win.title="Progress Bar", label="", maximum=100,
                          parent=NULL) {
@@ -20,15 +20,20 @@ ProgressBar <- function (win.title="Progress Bar", label="", maximum=100,
   }
 
   DestroyWindow <- function () {
+    tclServiceMode(FALSE)
     if (!.is.destroyed) {
       tkdestroy(.tt)
       .is.destroyed <<- TRUE
     }
+    tclServiceMode(TRUE)
   }
 
   SetLabel <- function (x) {
     tclvalue(.lab) <- x
   }
+
+
+  tclServiceMode(FALSE)
 
   .tt <-tktoplevel()
   if (!is.null(parent)) {
@@ -55,23 +60,28 @@ ProgressBar <- function (win.title="Progress Bar", label="", maximum=100,
   tkgrid(frame0.but, sticky="e")
   tkpack(frame0, fill="x", padx=10, pady=10)
 
+  tclServiceMode(TRUE)
+
   tkbind(.tt, "<Destroy>", DestroyWindow)
   tkfocus(.tt)
 
   lst <- list(GetValue=GetValue, MoveProgressBar=MoveProgressBar,
               SetLabel=SetLabel, DestroyWindow=DestroyWindow,
-              GetWindowState=GetWindowState, label=label)
+              GetWindowState=GetWindowState)
   return(structure(lst, class="ProgressBar"))
 }
 
 
 SetProgressBar <- function (pb, value, label=NULL) {
+  tclServiceMode(FALSE)
+  on.exit(tclServiceMode(TRUE))
   if (!inherits(pb, "ProgressBar"))
-    stop()
+    stop("invalid class for 'pb'")
+  if (!pb$GetWindowState())
+    return(NA)
   old.value <- pb$GetValue()
   pb$MoveProgressBar(value)
   if (!is.null(label))
     pb$SetLabel(label)
-  tcl("update", "idletasks")
-  return()
+  invisible(old.value)
 }
